40666b40b9a84aca05cfb1c681efb4ff5b369548
Linus Brennecke <linus.brennecke@atos.net>
2023-01-13 10:20:44 +0100

    added videos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,29 @@
-Horizontal View
+<!--
+<script>
+    import {videojs} from 'video.js'
+    const player = videojs('vid1', {});
+</script>
+
+<div class="section">
+	<div class="container">
+		<h1 class="is-size-1">Horizontal View</h1>
+
+        <video-js id="vid1">
+            <source src="//vjs.zencdn.net/v/oceans.mp4">
+        </video-js>
+
+		<p>
+			Video by <a
+				href="https://pixabay.com/users/padrinan-1694659/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=video&amp;utm_content=64759"
+				>Miguel Á. Padriñán</a
+			>
+			from
+			<a
+				href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=video&amp;utm_content=64759"
+				>Pixabay</a
+			>
+		</p>
+	</div>
+</div>
+-->
+horizontal
