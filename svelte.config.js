38f8666bb04841a8faa9ad4bb5d6773804e2e47d
Linus Brennecke <li.brennecke@web.de>
2023-02-26 09:58:50 +0100

    prepare for static site

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: dev ? '' : '/your-repo-name',
+		}
 	}
 };
 
