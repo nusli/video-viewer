3520c5c09f72dcd51b06216e1d1394b888b57d60
Linus Brennecke <linus.brennecke@atos.net>
2023-03-01 11:15:02 +0100

    static site in docs folder

---
+++ docs/_app/immutable/chunks/2-bf6c77a4.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-1c9f2dee.js";export{t as component};
---
+++ docs/_app/immutable/chunks/4-e02c60ed.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/vertical/_page.svelte-be721916.js";export{t as component};
